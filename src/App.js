5b094c3d45de9b20810ed12507625db8cd2bb415
Enzo Kieran <enzokamtchouang@gmail.com>
2024-09-01 01:36:07 +0100

    committed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
 
 function App() {
+  const [text, setText] = useState(`# Welcome to my Markdown Previewer!\n
+  -------
+  ## This is a sub heading...\n
+  ------
+  ### And here's some other cool stuff:\n
+  ------
+  Here comes some code, \`<div></div>\`, between 2 backticks.
+  \`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+You can also make a text **bold**... whoa!
+
+Or _italic_.\
+
+Or... wait for it... **_both!_**
+
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course, there are lists
+  - Some are bulleted.
+    - With differend indentation levels.
+      - That look like this.
+1. And there are number list too.
+2. Use just one's if you want
+1. And last but not least, let's not forget embedded images
+
+
+
+  `);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editor">
+        <div className="nav">
+          <h1>Editor</h1>
+        </div>
+        <textarea rows="55" cols="70" value={text} onChange={handleChange} spellCheck={false}></textarea>
+      </div>
+      <div className="results">
+        <div className="nav">
+          <h1>Results</h1>
+        </div>
+        <div className="result-display">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{text}</ReactMarkdown>
+        </div>
+        
+      </div>
     </div>
   );
 }
